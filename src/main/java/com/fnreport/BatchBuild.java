6bc5ac7bbb531ff501c26cf9d018d9abb0348711
jim <northrup.james@gmail.com>
2015-03-27 12:12:32 -0700

    testing per-query extraction

--- src/main/java/com/fnreport/BatchBuild.java
+++ src/main/java/com/fnreport/BatchBuild.java
@@ -1,14 +1,12 @@
 package com.fnreport;
 
 import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.sql.*;
-import java.text.MessageFormat;
 import java.util.*;
 import java.util.Date;
 
@@ -27,107 +25,30 @@ public class BatchBuild {
     static long counter;
 
     static public void main(String... args) throws IllegalAccessException, InstantiationException, SQLException, IOException {
-        String jdbcurl = null;
-        Driver DRIVER;
-        jdbcurl = args.length > 5 ? args[5] : MessageFormat.format("jdbc:mysql://{0}/{1}?zeroDateTimeBehavior=convertToNull&user={2}&password={3}", args[0], args[1], args[2], args[3]);
-        DRIVER = DriverManager.getDriver(jdbcurl);
-
-        Connection connect = DRIVER.connect(jdbcurl, new Properties());
-        DatabaseMetaData metaData = connect.getMetaData();
-
-        ResultSet sourceTables = metaData.getTables(null, null, null, new String[]{"TABLE"});
 
 
-        List<String> tables = new ArrayList<String>();
-        int c = 1;
-        while (sourceTables.next()) {
-            String table_schem = sourceTables.getString("TABLE_SCHEM");
-            String table_name = sourceTables.getString("TABLE_NAME");
-            if (table_schem != null && !table_schem.isEmpty()) {
-                table_name = table_schem + '.' + table_name;
-            }
-            {
-                tables.add(table_name);
-            }
-        }
+        final String name = args[0];
+        final String sql = args[1];
+        final String pkname = args[2];
+        final String couchPrefix = args[3];
+        final String url = args[4];
+        final ResultSet resultSet = DriverManager.getDriver(url).connect(url, new Properties()).createStatement().executeQuery(sql);
+        doSelect(couchPrefix, name, resultSet, pkname);
+    }
 
-//        Map<String, Map> rows = new LinkedHashMap<String, Map>();
-        Gson gson = BUILDER.create();//new GsonBuilder().setPrettyPrinting().create();
+    private static void doSelect(String couchPrefix, String name, ResultSet resultSet1, String pkname) throws SQLException, IOException {
+        ResultSetMetaData metaData1 = resultSet1.getMetaData();
+        int columnCount = metaData1.getColumnCount();
 
-        for (String tablename : tables) {
-
-            String[] split = tablename.split("\\.", 2);
-            String name = split.length > 1 ? split[1] : tablename;
-            Statement statement = connect.createStatement();
-            {
-
-                ResultSet resultSet = statement.executeQuery("select count(*) from " + tablename);
-                                      resultSet.next();
-                final long aLong = resultSet.getLong(1);
-                System.err.println("table: "+tablename+" "+aLong);
-                resultSet.close();
-
-            }
-            ResultSet resultSet = statement.executeQuery("select * from " + tablename);
-            ResultSetMetaData metaData1 = resultSet.getMetaData();
-            int columnCount = metaData1.getColumnCount();
-
-            String pk = null;
-            try {
-                ResultSet primaryKeys = metaData.getPrimaryKeys(null, split.length > 1 ? split[0] : null, name);
-                primaryKeys.next();
-                pk = primaryKeys.getString(4);
-            } catch (SQLException e) {
-
-            }
-            boolean first = true;
-            while (resultSet.next()) {
-                final String couchprefix = args[4];
-                if (first) {
+        boolean first = true;
+        while (resultSet1.next()) {
+            if (first) {
 //                             String str = (pk == null) ? Long.toHexString((++counter) | 0x1000000000l).substring(1) : resultSet.getString(pk);
-                    first = false;
-                    String spec = new StringBuilder().append(couchprefix) .append(name).toString();
-                    URL url = new URL(spec);
-                    HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-                    byte[] utf8s = "{}".getBytes();
-//                httpCon.getRequestProperties().put("Content-Type", asList("application/json"))  ;
-                    httpCon.setFixedLengthStreamingMode(utf8s.length);
-                    httpCon.setRequestMethod("PUT");
-                    httpCon.setUseCaches(true);
-                    httpCon.setDoOutput(true);
-//                gson.toJson(Arrays.asList(url, row), System.out);
-                    httpCon.getOutputStream().write(utf8s);
-                    httpCon.getOutputStream().flush();
-                    httpCon.disconnect();
-                }
-                Map row = new LinkedHashMap();
-
-                for (int i = 1; i < columnCount + 1; ++i) {
-                    String columnType = metaData1.getColumnClassName(i);
-                    String columnName = metaData1.getColumnName(i);
-                    try {
-                        Object object = resultSet.getObject(i);
-                        if (object instanceof Date) {
-                            Date date = (Date) object;
-
-
-                            object= date.getTime();
-                        }
-                        row.put(columnName, object);
-
-                    } catch (SQLException e) {
-                        System.err.println("cannot store " + columnType + " as column " + columnName + " with value ");
-                        row.put(columnName, resultSet.getString(i));
-                    }
-                }
-
-
-                final String string = resultSet.getString(pk);
-                String _id = (pk == null) ? Long.toHexString((++counter) | 0x1000000000l).substring(1) : string;
-                String spec = new StringBuilder().append(couchprefix) .append(name).append("/").append(_id).toString();
+                first = false;
+                String spec = new StringBuilder().append(couchPrefix) .append(name).toString();
                 URL url = new URL(spec);
                 HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-                byte[] utf8s = gson.toJson(row).getBytes("UTF8");
+                byte[] utf8s = "{}".getBytes();
 //                httpCon.getRequestProperties().put("Content-Type", asList("application/json"))  ;
                 httpCon.setFixedLengthStreamingMode(utf8s.length);
                 httpCon.setRequestMethod("PUT");
@@ -138,6 +59,43 @@ public class BatchBuild {
                 httpCon.getOutputStream().flush();
                 httpCon.disconnect();
             }
+            Map row = new LinkedHashMap();
+
+            for (int i = 1; i < columnCount + 1; ++i) {
+                String columnType = metaData1.getColumnClassName(i);
+                String columnName = metaData1.getColumnName(i);
+                try {
+                    Object object = resultSet1.getObject(i);
+                    if (object instanceof Date) {
+                        Date date = (Date) object;
+
+
+                        object= date.getTime();
+                    }
+                    row.put(columnName, object);
+
+                } catch (SQLException e) {
+                    System.err.println("cannot store " + columnType + " as column " + columnName + " with value ");
+                    row.put(columnName, resultSet1.getString(i));
+                }
+            }
+
+
+            final String string = resultSet1.getString(pkname);
+            String _id = (pkname == null) ? Long.toHexString((++counter) | 0x1000000000l).substring(1) : string;
+            String spec = new StringBuilder().append(couchPrefix) .append(name).append("/").append(_id).toString();
+            URL url = new URL(spec);
+            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+            byte[] utf8s = BUILDER.create().toJson(row).getBytes("UTF8");
+//                httpCon.getRequestProperties().put("Content-Type", asList("application/json"))  ;
+            httpCon.setFixedLengthStreamingMode(utf8s.length);
+            httpCon.setRequestMethod("PUT");
+            httpCon.setUseCaches(true);
+            httpCon.setDoOutput(true);
+//                gson.toJson(Arrays.asList(url, row), System.out);
+            httpCon.getOutputStream().write(utf8s);
+            httpCon.getOutputStream().flush();
+            httpCon.disconnect();
         }
     }
 
