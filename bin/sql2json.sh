5a2dd00ca87658fce7f222db2043a074fff04009
James Northrup <jim@vsiwest.com>
2019-04-12 17:24:38 +0700

    * java11 isBlank used. tweak sql2json script.

--- bin/sql2json.sh
+++ bin/sql2json.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-#HAZELCAST_SUPPORT=( --add-modules java.se
-#    {--add-exports' 'java.base/jdk.internal.ref,--add-opens' '{java.{base/{java.{lang,nio},sun.nio.ch},management/sun.management},jdk.management/com.sun.management.internal}}=ALL-UNNAMED
-#    )
 set -x
 JDIR=$(dirname $0)/../
-java  ${HAZELCAST_SUPPORT[@]} -classpath "$JDIR./target/*:$JDIR./target/lib/*"  ${EXECMAIN:=com.fnreport.BatchBuild} $@
+java  -classpath "$JDIR/target/*:$JDIR/target/lib/*"  ${EXECMAIN:=com.fnreport.BatchBuild} "$1" "$2" "$3" "$4" "$5"
--- src/main/java/com/fnreport/BatchBuild.java
+++ src/main/java/com/fnreport/BatchBuild.java
@@ -3,9 +3,9 @@ package com.fnreport;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.GsonBuilder;
 
-import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.sql.*;
 import java.text.MessageFormat;
 import java.util.*;
@@ -29,30 +29,28 @@ public class BatchBuild {
                     FieldNamingPolicy.IDENTITY).setPrettyPrinting();
     static long counter;
 
-    static public void main(String... args) throws   Exception {
+    static public void main(String... args) throws Exception {
         if (args.length < 1) {
             System.err.println(MessageFormat.format("convert a query to json (and PUT to url) \n{0} name ''sql'' pkname couch_prefix ''jdbc-url''", BatchBuild.class.getCanonicalName()));
-        exit(1);}
+            exit(1);
+        }
 
-        final String name = args[0];
+        final String couchDbName = args[0];
         final String sql = args[1];
         final String pkname = args[2];
         final String couchPrefix = args[3];
-        final String url = args[4];
-        final ResultSet resultSet = DriverManager.getDriver(url).connect(url, new Properties()).createStatement().executeQuery(sql);
-        doSelect(couchPrefix, name, resultSet, pkname);
-    }
-
-    private static void doSelect(String couchPrefix, String name, ResultSet resultSet1, String pkname) throws SQLException, IOException {
-        ResultSetMetaData metaData1 = resultSet1.getMetaData();
+        final String jdbcUrl = args[4];
+        Driver DRIVER = DriverManager.getDriver(jdbcUrl);
+        final ResultSet resultSet = DRIVER.connect(jdbcUrl, new Properties()).createStatement().executeQuery(sql);
+        ResultSetMetaData metaData1 = resultSet.getMetaData();
         int columnCount = metaData1.getColumnCount();
 
         boolean first = true;
-        while (resultSet1.next()) {
+        while (resultSet.next()) {
             if (first) {
 //                             String str = (pk == null) ? Long.toHexString((++counter) | 0x1000000000l).substring(1) : resultSet.getString(pk);
                 first = false;
-                String spec = new StringBuilder().append(couchPrefix).append(name).toString();
+                String spec = new StringBuilder().append(couchPrefix).append(couchDbName).toString();
                 URL url = new URL(spec);
                 HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
                 byte[] utf8s = "{}".getBytes();
@@ -72,7 +70,7 @@ public class BatchBuild {
                 String columnType = metaData1.getColumnClassName(i);
                 String columnName = metaData1.getColumnName(i);
                 try {
-                    Object object = resultSet1.getObject(i);
+                    Object object = resultSet.getObject(i);
                     if (object instanceof Date) {
                         Date date = (Date) object;
 
@@ -83,17 +81,16 @@ public class BatchBuild {
 
                 } catch (SQLException e) {
                     System.err.println("cannot store " + columnType + " as column " + columnName + " with value ");
-                    row.put(columnName, resultSet1.getString(i));
+                    row.put(columnName, resultSet.getString(i));
                 }
             }
 
 
-            final String string = resultSet1.getString(pkname);
-            String _id = (pkname == null) ? Long.toHexString((++counter) | 0x1000000000l).substring(1) : string;
-            String spec = new StringBuilder().append(couchPrefix).append(name).append("/").append(_id).toString();
+            String _id = pkname.isBlank() ? Long.toHexString((++counter) | 0x1000000000l).substring(1) : resultSet.getString(pkname);
+            String spec = new StringBuilder().append(couchPrefix).append(couchDbName).append("/").append(_id).toString();
             URL url = new URL(spec);
             HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-            byte[] utf8s = BUILDER.create().toJson(row).getBytes("UTF8");
+            byte[] utf8s = BUILDER.create().toJson(row).getBytes(StandardCharsets.UTF_8);
 //                httpCon.getRequestProperties().put("Content-Type", asList("application/json"))  ;
             httpCon.setFixedLengthStreamingMode(utf8s.length);
             httpCon.setRequestMethod("PUT");
@@ -105,5 +102,4 @@ public class BatchBuild {
             httpCon.disconnect();
         }
     }
-
 }
