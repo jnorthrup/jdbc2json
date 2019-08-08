0fdb415c9e355bbc67ddb7ad2be59bb269296a3c
Jim Northrup <jim@vsiwest.com>
2019-08-08 22:15:14 +0700

    compensate for informix lowercase noncompliance with sql spec

--- src/main/java/com/fnreport/QueryToFlat.kt
+++ src/main/java/com/fnreport/QueryToFlat.kt
@@ -46,7 +46,7 @@ class QueryToFlat {
                     val sql = asList(*args).subList(1, args.size).joinToString(" ")
 
                     System.err.println("using sql: $sql")
-                    val meta = mutableMapOf<String, Map<String,Map<String, Any?>>>()
+                    val meta = mutableMapOf<String, Map<String, Map<String, Any?>>>()
                     try {
                         val DRIVER = DriverManager.getDriver(jdbcUrl)
                         sqlms = measureTimeMillis {
@@ -60,17 +60,28 @@ class QueryToFlat {
 
                                 conn.metaData.getColumns(null, null, tname, null).also { rs: ResultSet ->
                                     val x = mutableListOf<String>()
-                                    (1..rs.metaData.columnCount).forEach { cnum: Int -> x += rs.metaData.getColumnName(cnum) }
+                                    (1..rs.metaData.columnCount).forEach { cnum: Int -> x += rs.metaData.getColumnName(cnum).toUpperCase() }
                                     meta[tname] = generateSequence {
                                         takeIf { rs.next() }?.let {
-                                            if (rs.row == 1) System.err.println("potential meta" to x);
-                                            rs.getString("column_name")to x.map { mname -> mname to rs.getObject(mname) }.toMap<String, Any?>()
+                                            if (rs.row == 1) System.err.println("potential meta" to x)
+                                            rs.getString("COLUMN_NAME") to
+                                                    x.map { mname ->
+                                                        mname.toUpperCase() to
+                                                                rs.getObject(mname)
+                                                    }.toMap<String, Any?>()
                                         }
                                     }.toMap()
-
                                 }
                             }
-                            System.err.println(meta.map {(k,v)-> k to v.map{ (k,m) ->   m["column_name"] to (JDBCType.values() [m["sql_data_type"] as Int] to m["char_octet_length"])}  })
+                            System.err.println(meta.map { (k, v) ->
+                                k to
+                                        v.map { (k, m) ->
+                                            m["COLUMN_NAME"] to
+                                                    (JDBCType.values()[
+                                                            m["SQL_DATA_TYPE"] as Int] to
+                                                            m["CHAR_OCTET_LENGTH"])
+                                        }
+                            })
 
                             val rs = conn.createStatement().executeQuery(sql)
                             val metaData = rs.metaData
@@ -78,7 +89,7 @@ class QueryToFlat {
 
                             data class flatSchemaColMeta(val name: String, val startcol: Int, val fieldLen: Int, val sqlType: SQLType)
 
-                            var lastlen = 0;
+                            var lastlen = 0
 
 
                             (1..metaData.columnCount).map { cnum ->
