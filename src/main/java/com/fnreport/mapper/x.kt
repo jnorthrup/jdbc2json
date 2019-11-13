640e784ec716154cf535ec593a242da6fee414c7
Jim Northrup <jim@vsiwest.com>
2019-11-13 16:50:43 +0700

    ng

--- src/main/java/com/fnreport/mapper/x.kt
+++ src/main/java/com/fnreport/mapper/x.kt
@@ -1,67 +1,68 @@
-
 package com.fnreport.mapper
 
 import java.io.Closeable
 import java.io.RandomAccessFile
+import java.nio.ByteBuffer
 import java.nio.MappedByteBuffer
 import java.nio.channels.FileChannel
 
-typealias Column=Pair<String,(Any?)->Any?>
-typealias Coordinates=Pair<Int,Int>
-typealias Decoder=Triple<Column,( ByteArray)->Any?,Coordinates>
-interface RowStore {
+typealias Column = Pair<String, (Any?) -> Any?>
+typealias Coordinates = Pair<Int, Int>
+typealias Decoder = Triple<Column, (ByteArray) -> Any?, Coordinates>
 
-/**
+interface RowStore<T> {
+
+    /**
      * seek to row
      */
 
-    operator fun invoke(row: Int): List< Any?>
+    operator fun invoke(row: Int): T
 }
 
 
-
-interface FixedRowStore : RowStore {
+interface FixedRowStore<T> : RowStore<T> {
     val recordLen: Int
     val size: Int
 }
-interface ColumnAccess {
-    operator fun get(vararg c: Int): RowStore
+
+interface ColumnAccess<T> {
+    operator fun get(vararg c: Int): RowStore<T>
     val columns: List<Column>
 }
 
-abstract class FileAccess(open val codex: List<Decoder>, open val filename: String) : Closeable {
-
-}
+abstract class FileAccess(open val filename: String) : Closeable
 
 
-
-interface IDataFrame : RowStore, ColumnAccess{
-}
-
+interface IDataFrame<T> : RowStore<T>, ColumnAccess<T>
 
 
 //todo: map multiple segments for a very big file
-class MappedFwf(
-        codex: List<Decoder>,
+open class MappedFile(
+//        codex: List<Decoder>,
         filename: String,
         randomAccessFile: RandomAccessFile = RandomAccessFile(filename, "r"),
         channel: FileChannel = randomAccessFile.channel,
         length: Long = randomAccessFile.length(),
-        private val mappedByteBuffer: MappedByteBuffer = channel.map(FileChannel.MapMode.PRIVATE, 0, length),
-        override val recordLen: Int = mappedByteBuffer.run {
-            var c = 0.toByte();
-            do c = get() while (c != '\n'.toByte())
-            position()
-        },
-        override val size: Int = (recordLen / length).toInt()
-) : FixedRowStore, FileAccess(codex, filename),Closeable by randomAccessFile {
-    override fun invoke(row: Int): List<Any?> { (row * recordLen).let { offset ->                            ByteArray(end - begin).also {
-        mappedByteBuffer.apply { position(offset + begin) }[it]
-    }
+        val mappedByteBuffer: MappedByteBuffer = channel.map(FileChannel.MapMode.PRIVATE, 0, length)
+//        val recordLen: Int = mappedByteBuffer.run {
+//            var c = 0.toByte();
+//            do c = get() while (c != '\n'.toByte())
+//            position()
+//        },
+//        val size: Int = (recordLen / length).toInt()//d functions use File | Settings | File Templates.
+) : RowStore<ByteBuffer>, FileAccess(
+        filename), Closeable by randomAccessFile {
+    override fun invoke(row: Int): ByteBuffer = mappedByteBuffer.apply { position(row) }.slice()
+}
 
 
+//todo: map multiple segments for a very big file
 
-};
+class FixedLineStore(/*override val columns: List<Column> =listOf(  Column("line", { any: Any? -> (any as? Lazy<*>)?.value ?: any })),*/
+                     val origin: MappedFile, override val recordLen: Int, override val size: Int) : FixedRowStore<Lazy<ByteBuffer>> {
+    override fun invoke(row: Int): Lazy<ByteBuffer> = lazyOf(origin.invoke(row * recordLen).limit(recordLen) as ByteBuffer)
+//    override fun get(vararg c: Int)  =this;
+}
 
 
 /*
