ed8c7bd077bf1df33dbba027a06dea6b99344d9b
Jim Northrup <jim@vsiwest.com>
2019-11-14 16:53:34 +0700

    Columnar

--- src/main/java/com/fnreport/mapper/x.kt
+++ src/main/java/com/fnreport/mapper/x.kt
@@ -1,26 +1,31 @@
 package com.fnreport.mapper
 
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flowOf
 import java.io.Closeable
 import java.io.RandomAccessFile
 import java.nio.ByteBuffer
 import java.nio.MappedByteBuffer
 import java.nio.channels.FileChannel
 
-typealias Column = Pair<String, (Any?) -> Any?>
 typealias Coordinates = Pair<Int, Int>
-typealias Decoder = Triple<Column, Coordinates, (ByteArray, Coordinates) -> Any?>
 
 
 interface RowStore<T> {
-
     /**
      * seek to row
      */
+    fun values(row: Int): T
 
-    operator fun invoke(row: Int): T
+    val size: Int
+}
+
+interface FlowStore<T> {
+    /**
+     * seek to row
+     */
+    suspend fun values(row: Int): T
 
     val size: Int
 }
@@ -29,18 +34,8 @@ interface RowStore<T> {
 interface FixedLength<T> : Indexed<T> {
     val recordLen: Int
 }
-/*
-interface ColumnAccess<T> {
-    operator fun get(vararg c: Int): RowStore<T>
-    val columns: List<Column>
-}*/
 
 abstract class FileAccess(open val filename: String) : Closeable
-
-/*
-
-interface IDataFrame<T> : RowStore<T>, ColumnAccess<T>
-*/
 
 
 //todo: map multiple segments for a very big file
@@ -53,7 +48,7 @@ open class MappedFile(
         override val size: Int = mappedByteBuffer.limit()
 ) : RowStore<ByteBuffer>, FileAccess(
         filename), Closeable by randomAccessFile {
-    override fun invoke(row: Int): ByteBuffer = mappedByteBuffer.apply { position(row) }.slice()
+    override fun values(row: Int): ByteBuffer = mappedByteBuffer.apply { position(row) }.slice()
 }
 
 /**
@@ -81,17 +76,41 @@ open class FixedRecordLengthBuffer(val buf: ByteBuffer,
                                    },
 
                                    val size: Int = (recordLen / buf.limit())
-) : LineBuffer(),
-        FixedLength<Flow<ByteBuffer>> {
+) : LineBuffer(), FixedLength<Flow<ByteBuffer>> {
     override fun get(vararg rows: Int) = rows.map { buf.position(recordLen * it).slice().apply { limit(recordLen) } }.asFlow()
 
 }
 
+/**
+ * our tight recursive object with flexible column features
+ */
+class Columnar(var rs: RowStore<ByteBuffer>, val columns: List<Pair<String, Pair<Pair<Int, Int>,(ByteArray)->Any?>>>) : FlowStore<Flow<List<*>>> {
+
+    override val size: Int
+        get() = rs.size
+
+    operator fun get(cols: IntArray): Columnar {
+        return Columnar(this.rs, cols.map { columns[it] })
+    }
+
+    override suspend fun values(row: Int) =
+            rs.values(row).let { rs1 ->
+                flowOf(columns.map { (a, mapper) ->
+                    val (coor,conv)=mapper
+                    val (begin, end) = coor
+                    val len = end - begin
+                    val fb = rs1.position(begin).slice().limit(len)
+                    conv(ByteArray(len).also { fb.get(it) })
+
+                })
+            }
+}
+
 open class VariableRecordLengthFile(filename: String, origin: MappedFile = MappedFile(filename)) : Closeable by origin, VariableRecordLengthBuffer(buf = origin.mappedByteBuffer)
 
-open class VariableRecordLengthBuffer(val buf: ByteBuffer, val header:Boolean=false,val eor: Char = '\n', val index: IntArray = buf.duplicate().clear().run {
+open class VariableRecordLengthBuffer(val buf: ByteBuffer, val header: Boolean = false, val eor: Char = '\n', val index: IntArray = buf.duplicate().clear().run {
     val list = mutableListOf<Int>()
-    if(!header)list+=position()
+    if (!header) list += position()
 
     var c = 0.toChar()
     while (hasRemaining()) {
@@ -100,35 +119,54 @@ open class VariableRecordLengthBuffer(val buf: ByteBuffer, val header:Boolean=fa
             list += position()
     }
     list.toIntArray()
-}, val size: Int = index.size) : LineBuffer() {
+
+}, override val size: Int = index.size
+) : LineBuffer(), RowStore<ByteBuffer> {
     override fun get(vararg rows: Int) =
             rows.map { row: Int ->
-                buf.position(index[row]).slice().also {
-                    if (row != index.size - 1) {
-                        it.limit(index[row + 1] - index[row] - 1)
-                    }
-                }
+                this.values(row)
             }.asFlow()
+
+    override fun values(row: Int): ByteBuffer = buf.position(index[row]).slice().also {
+        if (row != index.size - 1) {
+            val i = index[row + 1] - index[row] - 1
+            it.limit(i)
+        }
+    }
 }
 
 
+/* tedious for now.   will revisit.
+
 @UseExperimental(InternalCoroutinesApi::class)
-class CsvFile(fn: String, delim: CharArray = charArrayOf('\n', ','), val header: Boolean=true, val fileBuf: VariableRecordLengthFile = VariableRecordLengthFile(fn)) : Indexed<Flow<Flow<VariableRecordLengthBuffer>>> {
+class CsvFile(
+        fn: String,
+        delim: CharArray = charArrayOf('\n', ','),
+        val header: Boolean = true,
+        val fileBuf: VariableRecordLengthFile = VariableRecordLengthFile(fn)
+) : Indexed<Flow<VariableRecordLengthBuffer>>, RowStore<ByteBuffer> by fileBuf {
 
     lateinit var columns: List<String>
 
     init {
         runBlocking {
-            val codexBuf = fileBuf[0].first()
-            val row0 = VariableRecordLengthBuffer(codexBuf,  eor=',')
-            columns = row0[0 until row0.size].map { b ->
-                String(ByteArray(b.remaining()).also { z -> b.get(z) })
-            }.toList()
+            val codexBuf = fileBuf(0)
+            val byteArray = ByteArray(codexBuf.limit())
+            codexBuf.duplicate().get(byteArray)
+            val string = String(byteArray)
+//            val headerRow = VariableRecordLengthBuffer(codexBuf, false, delim[1])
+//            val size1 = headerRow.size
+//            columns = headerRow[0 until size1].map { b ->
+//                String(ByteArray(b.remaining()).also { z -> b.get(z) })
+//            }.toList()
+            columns=string.split("\\W+")
         }
     }
 
-      override operator fun   get(vararg rows: Int)  = fileBuf.get(*rows).map{ flowOf( VariableRecordLengthBuffer(it,eor=','))}
-
-
+    override operator fun get(vararg rows: Int) = fileBuf.get(*rows).map { flowOf(VariableRecordLengthBuffer(it, eor = ',')) }
 }
 
+
+
+
+*/
